ae8c744c7fdb8bbba9e9c5703edbf9b7dc0f50fa
Nomlees <nomlees@bk.ru>
2017-12-03 22:37:21 +0300

    Класс копирования из файла

---
+++ src/ru/zak/copyfiles/Copy.java
@@ -0,0 +1,33 @@
+package ru.zak.copyfiles;
+
+import java.io.*;
+
+
+
+public class Copy extends Thread{
+
+    private String adress;
+    private String output;
+
+    Copy(String adress, String output) {
+        this.adress = adress;
+        this.output = output;
+    }
+
+    public void run() {
+        String string;
+        long before = System.currentTimeMillis();
+        try(BufferedReader reader = new BufferedReader(new FileReader(adress));
+        BufferedWriter write = new BufferedWriter(new FileWriter(output))){
+            while ((string = reader.readLine()) != null){
+                write.write(string+ "\n");
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("Копирование: "+ (System.currentTimeMillis() - before) +" милисекунд(ы)");
+
+    }
+}
